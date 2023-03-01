459cb6403f46caf2c6d9a6e240894e1e65a87e08
Rajashree Sonawane <rajashrees@synerzipune.local>
2023-03-01 12:21:37 +0530

    implemented data driven testing

--- MagnetoCypressTesting/cypress/e2e/MyTest/Magnetotest.cy.js
+++ MagnetoCypressTesting/cypress/e2e/MyTest/Magnetotest.cy.js
@@ -1,15 +1,20 @@
 Cypress.LocalStorage.clear = function (keys, ls, rs) { };
 describe('Magneto Software Testing', () => {
-  let testdatag;
   let productTestg
+  let data
+  let rowsLength
   before(function () {
-    cy.visit(Cypress.env('url'))
-    cy.fixture('test_data').then(function (testdata) {
-      testdatag = testdata
+    cy.task('readXlsx', { file: 'cypress/fixtures/MagnetoExcel.xlsx', sheet: "MagnetoData" }).then((rows) => {
+      rowsLength = rows.length;
+      cy.writeFile("cypress/fixtures/xlsxData.json", { rows })
+    })
+    cy.fixture('xlsxData').then((datatest) => {
+      data = datatest
     })
     cy.fixture('product').then(function (productTest) {
       productTestg = productTest
     })
+    cy.visit(Cypress.env('url'))
   })
   it('Choose Product for Magneto', function () {
     cy.get('#ui-id-4 > :nth-child(2)').realHover()
@@ -34,36 +39,45 @@ describe('Magneto Software Testing', () => {
     cy.wait(15000)
   })
   it('Filling shipping Data', () => {
-    cy.log(testdatag.email)
-    cy.get("#customer-email-fieldset > .required > .control > #customer-email").type(testdatag.email),
+    for (let i = 0; i < rowsLength; i++) {
+      cy.get("#customer-email-fieldset > .required > .control > #customer-email").type(data.rows[i].email);
+      cy.get("#customer-email-fieldset > .required > .control > #customer-email").clear()
       cy.wait(10000)
-    cy.xpath("//input[@name='firstname']").type(testdatag.firstName),
+      cy.xpath("//input[@name='firstname']").type(data.rows[i].firstname)
+      cy.xpath("//input[@name='firstname']").clear()
       cy.wait(2000)
-    cy.xpath("//input[@name='lastname']").type(testdatag.lastName),
+      cy.xpath("//input[@name='lastname']").type(data.rows[i].lastname)
+      cy.xpath("//input[@name='lastname']").clear()
       cy.wait(2000)
-    cy.xpath("//input[@name='company']").type(testdatag.company),
+      cy.xpath("//input[@name='company']").type(data.rows[i].company)
+      cy.xpath("//input[@name='company']").clear()
       cy.wait(2000)
-    cy.xpath("//input[@name='street[0]']").type(testdatag.street_address),
+      cy.xpath("//input[@name='street[0]']").type(data.rows[i].street_address)
+      cy.xpath("//input[@name='street[0]']").clear()
       cy.wait(2000)
-    cy.xpath("//input[@name='city']").type(testdatag.city),
+      cy.xpath("//input[@name='city']").type(data.rows[i].city),
+      cy.xpath("//input[@name='city']").clear()
       cy.wait(2000)
-    cy.xpath("//input[@name='postcode']").type(testdatag.postal_code),
+      cy.xpath("//input[@name='postcode']").type(data.rows[i].postal_code)
+      cy.xpath("//input[@name='postcode']").clear()
       cy.wait(5000)
-    cy.get('select[name="country_id"]').select('India').should('have.value', 'IN')
-    cy.wait(15000)
-    cy.get('select[name="region_id"]').select('Andhra Pradesh')
-    cy.wait(10000)
-    cy.xpath("//input[@name='telephone']").type(testdatag.phone),
+      cy.get('select[name="country_id"]').select('India').should('have.value', 'IN')
+      cy.wait(15000)
+      cy.get('select[name="region_id"]').select('Andhra Pradesh')
+      cy.wait(10000)
+      cy.xpath("//input[@name='telephone']").type(data.rows[i].phone)
+      cy.xpath("//input[@name='telephone']").clear()
       cy.wait(2000)
-    cy.xpath("//*[@id='label_carrier_flatrate_flatrate']").click({ force: true }),
+      cy.xpath("//*[@id='label_carrier_flatrate_flatrate']").click({ force: true })
       cy.wait(2000)
-    cy.get(".button").click()
+      cy.get(".button").click()
+    }
   })
   it('Verify Order Summary data', function () {
-    cy.get('.opc-block-summary'),
-      cy.wait(5000)
-    cy.get('.block > .title').click(),
-      cy.wait(5000)
+    cy.get('.opc-block-summary')
+    cy.wait(5000)
+    cy.get('.block > .title').click()
+    cy.wait(5000)
     cy.log(productTestg.Product_name)
     cy.get('.product-item-name')
       .should('contain.text', productTestg.Product_name)
